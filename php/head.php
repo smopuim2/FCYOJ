79fd3a7418649f8b429b7bec053798cf8fc1e474
smopuim2 <114752061+smopuim2@users.noreply.github.com>
2024-03-06 17:19:04 +0800

    Update head.php

--- php/head.php
+++ php/head.php
@@ -1,2 +1,2 @@
 <?php
-echo '<a href="http://fcyxx.njgljy.com"><img src="//smopuim2.github.io/FCYOJ/img/fcyxx_logo.jpg" alt="南京市芳草园小学" style="width:100%; height:90px;" /></a>';
+echo '<img src="//smopuim2.github.io/FCYOJ/img/fcyoj_logo.png" alt="FCYOJ logo" style="width:50px;" />';
